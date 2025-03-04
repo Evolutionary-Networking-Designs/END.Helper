1f4f623952ec24eaf7c8f428a5ebb394a14e379a
Kiera Meredith <kiera@meredithkm.info>
2025-03-03 20:35:42 -0500

    Refactor Code

--- END.Helper/Config/Const.cs
+++
@@ -1,9 +0,0 @@
-// ReSharper disable once CheckNamespace
-namespace END.Helper;
-
-public static class Const
-{
-    public const string AppSettingsFile = "appsettings.json";
-    public const string EnvFile = "env.json";
-    public const string WebConfigFile = "web.config";
-}
